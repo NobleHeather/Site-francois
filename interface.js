c30541aadfd776d4dd578798f5679a2dff0626fc
Alice M-C <morgane_felloni@hotmail.fr>
2021-03-09 04:26:43 +0100

    09/03/2021 insomnie tableau local storage

--- interface.js
+++ interface.js
@@ -1,3 +1,4 @@
+// localStorage.clear();
 //on récupère la page du blog et on crée une structure vide
 let wrapperBlog = document.getElementById("wrapperBlog");
 let newArticle = document.createElement("div");
@@ -80,15 +81,15 @@ function AjouteContenu(key, texte) {
 //ajoute un objet avec titre + index dans le tableau des titres
 let obj = {};
 obj[key] = texte;
-tabContenu.push(obj);
+tabContenu.unshift(obj); //*anciennement push
 // stock l'objet dans local storage
 localStorage.setItem("newObject", JSON.stringify(tabContenu));
 // récupère l'objet depuis local storage
 let retrievedObject = localStorage.getItem("newObject");
-// console.log("retrievedObject: ", JSON.parse(retrievedObject));
+console.log("retrievedObject: ", retrievedObject);
     //affiche tous les titres et leur index
     for (let [key, value] of Object.entries(obj)) {
-        console.log(`${key}: ${value}`);
+        // console.log(`${key}: ${value}`);
         //cible juste l'index
         let justKey = `${key}`;
         // let justValue = `${value}`;
@@ -99,38 +100,92 @@ let retrievedObject = localStorage.getItem("newObject");
     //ici on a tabContenu qui contient (index + input) * 4  
     console.log('index + input :' + retrievedObject); //= tabContenu dans storage
 }
-let newObject = localStorage.getItem("newObject");
-console.log('index + input :' + newObject); //= tabContenu dans storage
-
+//!ajouter au début du tableau, mettre dans publication les 4 premiers éléments
 let publication = []; //tab pour stocker les articles
-let retrievedPublication = localStorage.getItem("newPublication");
+// let retrievedPublication = localStorage.getItem("newPublication");
+// console.log(retrievedPublication);
+let livre = []
 
-function AjoutePublication(index, tab) {
-
-    i = 0;
-    while (i < publication.length) {
-        console.log(i);
-        if (i != null) { //case remplie
-            i++;
-        } else { //remplir case
-            //met le tableau contenant l'article en entier dans une case du tableau publication
-            publication.push(newObject);
-            console.log(publication);
-            i++;
-            //! stocker dans storage de manière à avoir :
-            //! publication[article, article, article]
-
-        }
-    }
-    
-}
+localStorage.setItem("publication", JSON.stringify(publication));
+var test = JSON.parse(localStorage.getItem("publication"));
+console.log("publication avant boucle" + test);
 
 //quand on clique sur bouton publier un article, fonction se lance
 publierArticle.addEventListener('click', function() {
-    
-    AjoutePublication(?, newObject) // '?' doit être l'article. newObject ?
+    // let i = 0;
+    for (let i = -1; i < publication.length; i++)
+    if (publication[i] != null) {//case remplie
+        //on avance
+        i++;
+        console.log('if :' + i);
+    } else { //on rempli la case
+        let newObject = JSON.parse(localStorage.getItem("newObject"));
+        console.log(newObject);
+        //! récup tableau, push new Object, renvoie tab
+        publication = JSON.parse(localStorage.getItem("publication"));
+        console.log(publication);
+        for (let [key, value] of Object.entries(newObject)) { //!order not garanteed !
+            console.log(`${key}: ${value}`);
+            // publication.push()
+          }
+        console.log(newObject[0]);
+        console.log(newObject[1]);
+        console.log(newObject[2]);
+        console.log(newObject[3]);
+        // console.log(ninja.adresse.ville); //récupérer objet dans objet
+        // console.log(ninja['adresse']['ville']);//récupérer info (2e version)
+        //mettre ici les 4 premiers newObject
+        publication.unshift(newObject[0], newObject[1], newObject[2], newObject[3]); 
+        console.log(publication);
+        //!il faut capter la clé de livre et l'incrémenter avant de renvoyer livre sur local storage
+        //!existe un équivalent de push ?
+        localStorage.setItem("livre", JSON.stringify(livre)); 
+        livre = JSON.parse(localStorage.getItem("livre"));
+        livre.push([i, publication]); //* livre contient index + article en entier
+        console.log(livre);
+        // localStorage.getItem("publication");
+        // console.log("i" + i + "publication" + publication);
+        i++;
+    }
+
+
+    // AjoutePublication(k, newObject) // '?' doit être l'article. newObject ?
 
 });
+
+// let k = 0;
+// function AjoutePublication(index, tab) {
+
+    // console.log('index + input :' + newObject); //= tabContenu dans storage
+
+    // for (k = 0; k < publication.length; k++) {
+    //     console.log(k);
+    //     if (publication[k] != null) { //case remplie
+    //         console.log('if :' + publication);
+    //         k++;
+    //     } else { //remplir case
+    //         //met le tableau contenant l'article en entier dans une case du tableau publication
+    //         publication.push(newObject);
+    //         console.log(publication);
+    //         //! stocker dans storage de manière à avoir :
+    //         //! publication[article, article, article]
+    //         publication = JSON.stringify(publication); //formater pour storage
+    //         localStorage.setItem(k, publication); //dans tableau publications il y a plusieurs publication
+    //         publication[k] = JSON.parse(localStorage.getItem(k));
+    //         let storedPublications = JSON.parse(localStorage.getItem("publications"));
+    //         console.log(storedPublications);
+    //         k++;
+            
+            // var storedNames = JSON.parse(localStorage.getItem("names"));
+            // var val = JSON.stringify(data);
+            // window.localStorage.setItem(key, val);
+            // value = JSON.parse(window.localStorage.getItem(key));
+//         }
+//     }
+    
+// }
+
+
 // AjouteContenu (num, "yo"); 
 // AjouteContenu (num, "yoo"); 
 //* JUSQUE ICI
@@ -192,6 +247,7 @@ enregistrerArticle.addEventListener("click", function() {
 
     AjouteContenu(num, titleInput.value);
     AjouteContenu(num, resumeInput.value);
+    AjouteContenu(num, articleInput.value);
     AjouteContenu(num, dateInput.value);
     // let thisTitle = AjouteContenu(num, titleInput.value);
     // let thisResume = AjouteContenu(num, resumeInput.value);
